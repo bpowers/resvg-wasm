a61b4bcc246b13a47d3a9e35964e5e086b6f611c
Bobby Powers <bobbypowers@gmail.com>
2021-04-06 22:25:19 -0700

    compiles

--- build.sh
+++ build.sh
@@ -8,20 +8,20 @@ cd "$DIR"
 rm -rf lib lib.browser pkg pkg-node core
 
 # Get the package name
-PKG_NAME=${PWD##*/}
+PKG_NAME=resvg_wasm
 
 cargo build --lib --release --target wasm32-unknown-unknown
 
 echo "running wasm-bindgen"
-wasm-bindgen ../../target/wasm32-unknown-unknown/release/${PKG_NAME}.wasm --out-dir pkg --typescript --target bundler
-wasm-bindgen ../../target/wasm32-unknown-unknown/release/${PKG_NAME}.wasm --out-dir pkg-node --typescript --target nodejs
+wasm-bindgen ./target/wasm32-unknown-unknown/release/${PKG_NAME}.wasm --out-dir pkg --typescript --target bundler
+wasm-bindgen ./target/wasm32-unknown-unknown/release/${PKG_NAME}.wasm --out-dir pkg-node --typescript --target nodejs
 
 if [ "1" != "${DISABLE_WASM_OPT-0}" ]; then
   echo "running wasm-opt"
-  wasm-opt pkg/engine_bg.wasm -o pkg/engine_bg.wasm-opt.wasm -O3 --enable-mutable-globals
-  wasm-opt pkg-node/engine_bg.wasm -o pkg-node/engine_bg.wasm-opt.wasm -O3 --enable-mutable-globals
-  mv pkg/engine_bg.{wasm-opt.,}wasm
-  mv pkg-node/engine_bg.{wasm-opt.,}wasm
+  wasm-opt pkg/${PKG_NAME}_bg.wasm -o pkg/${PKG_NAME}_bg.wasm-opt.wasm -O3 --enable-mutable-globals
+  wasm-opt pkg-node/${PKG_NAME}_bg.wasm -o pkg-node/${PKG_NAME}_bg.wasm-opt.wasm -O3 --enable-mutable-globals
+  mv pkg/${PKG_NAME}_bg.{wasm-opt.,}wasm
+  mv pkg-node/${PKG_NAME}_bg.{wasm-opt.,}wasm
 else
   echo "skipping wasm-opt"
 fi
--- index.ts
+++ index.ts
@@ -2,15 +2,15 @@
 // Use of this source code is governed by the Mozilla Public
 // License, Version 2.0, that can be found in the LICENSE file.
 
-let cachedWasmModule: typeof import("./core/resvg-wasm") | undefined;
-function getWasmModule(): Promise<typeof import("./core/resvg-wasm")> {
+let cachedWasmModule: typeof import("./core/resvg_wasm") | undefined;
+function getWasmModule(): Promise<typeof import("./core/resvg_wasm")> {
   return new Promise((resolve, reject) => {
     if (cachedWasmModule) {
       resolve(cachedWasmModule);
       return;
     }
 
-    import("./core/resvg-wasm")
+    import("./core/resvg_wasm")
       .then((module) => {
         cachedWasmModule = module;
         resolve(module);
